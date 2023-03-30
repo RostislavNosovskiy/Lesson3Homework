a462b97c0ef47f4ec03699ee4eb32fcdff899e99
Rosislav <rostislavnosovskij467@gmail.com>
2023-03-31 00:02:14 +0300

    Домашнее задание к третьему семинару

---
+++ task23/Program.cs
@@ -0,0 +1,16 @@
+﻿/*** Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+Console.Clear();
+Console.WriteLine("Введите число:");
+int n = int.Parse(Console.ReadLine());
+int[] numbers = new int[n];
+int i = 0;
+for (i = 1; i<=n ; i++)
+{
+  numbers [i-1] = i*i*i;
+}
+Console.Write($"{n} -> ");
+for (i = 0; i<n ; i++)
+Console.Write($"{numbers[i]} ");
